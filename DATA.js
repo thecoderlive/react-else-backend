c499c7e5bbc4e69dfa38b5d84059313b8a82040e
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-15 18:43:39 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,57 @@
+export const item = {
+    "id": "9bd4ac07-7578-4e57-a585-c66c7b28bddf",
+    "story_list": [
+        {
+            "id": "9bdf3b36-d0e9-427a-bc5a-4e35a6bdf935",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/40.png",
+            "start_time": "space",
+            "end_time": "note"
+        },
+        {
+            "id": "8e7bae55-93e7-44de-b7c0-166a010a02a8",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/275.png",
+            "start_time": "seat",
+            "end_time": "doctor"
+        },
+        {
+            "id": "c015508a-d289-48f3-90d4-d619610cf030",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/16.png",
+            "start_time": "response",
+            "end_time": "art"
+        },
+        {
+            "id": "97f90621-0437-43e9-9479-e5e94bc811b9",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/276.png",
+            "start_time": "travel",
+            "end_time": "true"
+        },
+        {
+            "id": "8fb68d27-cebc-41ed-85bd-f8bb68347348",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/201.png",
+            "start_time": "child",
+            "end_time": "but"
+        }
+    ],
+    "stories": [
+        {
+            "id": "47dc1c0f-1e14-43dd-9db1-a5802010dd1a",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/349.png"
+        },
+        {
+            "id": "76ce4a48-de44-47da-a868-2aedc9019095",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+        },
+        {
+            "id": "6c816849-c520-4952-888b-e8528710fe8f",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/114.png"
+        },
+        {
+            "id": "95f96e7a-497d-43d9-bede-a7aa1fcb1f88",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+        },
+        {
+            "id": "9e2a0544-7eb1-43f7-a3c9-077e7db94ba5",
+            "story_image": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png"
+        }
+    ]
+}
